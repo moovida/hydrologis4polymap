36edc12f89d2fc9d88c628aaec27238203d44e94
Andrea Antonello <andrea.antonello@gmail.com>
2017-07-28 17:29:52 +0200

    add discharge modules group

--- com.hydrologis.polymap.jgt/src/com/hydrologis/polymap/jgt/JgtModuleProvider.java
+++ com.hydrologis.polymap.jgt/src/com/hydrologis/polymap/jgt/JgtModuleProvider.java
@@ -17,11 +17,22 @@ package com.hydrologis.polymap.jgt;
 import java.util.List;
 import java.util.Optional;
 
+import org.jgrasstools.gears.modules.r.cutout.OmsCutOut;
+import org.jgrasstools.hortonmachine.modules.basin.rescaleddistance.OmsRescaledDistance;
+import org.jgrasstools.hortonmachine.modules.basin.topindex.OmsTopIndex;
+import org.jgrasstools.hortonmachine.modules.demmanipulation.pitfiller.OmsDePitter;
+import org.jgrasstools.hortonmachine.modules.demmanipulation.pitfiller.OmsPitfiller;
+import org.jgrasstools.hortonmachine.modules.demmanipulation.wateroutlet.OmsExtractBasin;
+import org.jgrasstools.hortonmachine.modules.geomorphology.ab.OmsAb;
 import org.jgrasstools.hortonmachine.modules.geomorphology.aspect.OmsAspect;
+import org.jgrasstools.hortonmachine.modules.geomorphology.curvatures.OmsCurvatures;
+import org.jgrasstools.hortonmachine.modules.geomorphology.draindir.OmsDrainDir;
+import org.jgrasstools.hortonmachine.modules.geomorphology.flow.OmsFlowDirections;
+import org.jgrasstools.hortonmachine.modules.geomorphology.gradient.OmsGradient;
+import org.jgrasstools.hortonmachine.modules.hydrogeomorphology.peakflow.OmsPeakflow;
+import org.jgrasstools.hortonmachine.modules.network.extractnetwork.OmsExtractNetwork;
 
 import com.google.common.collect.Lists;
-import com.hydrologis.polymap.jgt.aoi.OmsAreaOfInterestGridCoverage;
-
 import org.polymap.core.data.process.ModuleInfo;
 import org.polymap.core.data.process.ModuleProvider;
 
@@ -38,7 +49,19 @@ public class JgtModuleProvider
         return Lists.newArrayList(
                 // Milestone "Go Live" modules
                 new JgtModuleInfo( OmsAspect.class ),
-                new JgtModuleInfo( OmsAreaOfInterestGridCoverage.class )
+                new JgtModuleInfo( OmsPitfiller.class ),
+                new JgtModuleInfo( OmsDePitter.class ),
+                new JgtModuleInfo( OmsFlowDirections.class ),
+                new JgtModuleInfo( OmsDrainDir.class ),
+                new JgtModuleInfo( OmsExtractNetwork.class ),
+                new JgtModuleInfo( OmsExtractBasin.class ),
+                new JgtModuleInfo( OmsCutOut.class ),
+                new JgtModuleInfo( OmsGradient.class ),
+                new JgtModuleInfo( OmsTopIndex.class ),
+                new JgtModuleInfo( OmsCurvatures.class ),
+                new JgtModuleInfo( OmsAb.class ),
+                new JgtModuleInfo( OmsRescaledDistance.class ),
+                new JgtModuleInfo( OmsPeakflow.class )
                 //...
         );
     }
